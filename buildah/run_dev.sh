7ea26489658972da93b313be6bc462c3008d267b
Charles Titus <ctitus@bnl.gov>
2024-05-21 00:23:38 -0400

    fixed local package paths, removed command to make run_dev hang -- just run something after it yourself

--- buildah/run_dev.sh
+++ buildah/run_dev.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 cd /usr/local/src
-pip3 install -e xraygui/nbs_core
-pip3 install -e xraygui/nbs_sim
+pip3 install -e xraygui/nbs-core
+pip3 install -e xraygui/nbs-sim
 
-tail -f /dev/null
