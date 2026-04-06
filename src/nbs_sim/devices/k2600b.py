76176c293c11a4ec97e410dfc8dfb60ddc29962c
Charles Titus <ctitus@bnl.gov>
2026-04-06 13:43:51 -0400

    Working K2600B sim object

--- src/nbs_sim/devices/k2600b.py
+++ src/nbs_sim/devices/k2600b.py
@@ -1,4 +1,6 @@
+from multiprocessing import Value
 from caproto.server import PVGroup, pvproperty
+import numpy as np
 
 
 class K2600B(PVGroup):
@@ -26,14 +28,14 @@ class K2600B(PVGroup):
     sts_out_ena = pvproperty(name="Sts:Out-Ena", value=1, dtype=int, read_only=True)
     cmd_out_ena = pvproperty(name="Cmd:Out-Ena", value=1, dtype=int)
 
-    rb_vlvl = pvproperty(name="RB-VLvl", value=0.0, dtype=float, read_only=True)
-    sp_vlvl = pvproperty(name="SP-VLvl", value=0.0, dtype=float)
+    rb_vlvl = pvproperty(name="RB-VLvl", value=0.0, dtype=float, read_only=True, precision=3)
+    sp_vlvl = pvproperty(name="SP-VLvl", value=0.0, dtype=float, precision=3)
 
-    rb_ilvl = pvproperty(name="RB-ILvl", value=0.0, dtype=float, read_only=True)
-    sp_ilvl = pvproperty(name="SP-ILvl", value=0.0, dtype=float)
+    rb_ilvl = pvproperty(name="RB-ILvl", value=0.0, dtype=float, read_only=True, precision=3)
+    sp_ilvl = pvproperty(name="SP-ILvl", value=0.0, dtype=float, precision=3)
 
-    rb_meas_v = pvproperty(name="RB-MeasV", value=0.0, dtype=float, read_only=True)
-    rb_meas_i = pvproperty(name="RB-MeasI", value=1e-9, dtype=float, read_only=True)
+    rb_meas_v = pvproperty(name="RB-MeasV", value=0.0, dtype=float, read_only=True, precision=3)
+    rb_meas_i = pvproperty(name="RB-MeasI", value=0.0, dtype=float, read_only=True, precision=3)
 
     sts_meas_irang = pvproperty(
         name="Sts-MeasIRang", value=0.0, dtype=float, read_only=True
@@ -63,17 +65,27 @@ class K2600B(PVGroup):
     @cmd_out_ena.putter
     async def cmd_out_ena(self, instance, value):
         await self.sts_out_ena.write(int(value))
+        if value == 0:
+            await self.rb_meas_v.write(0)
+            await self.rb_meas_i.write(0)
+        else:
+            await self.rb_meas_v.write(self.rb_vlvl.value)
+            await self.rb_meas_i.write(self.rb_ilvl.value)
         return value
 
     @sp_vlvl.putter
     async def sp_vlvl(self, instance, value):
         await self.rb_vlvl.write(value)
-        await self.rb_meas_v.write(value)
+
         return value
 
     @sp_ilvl.putter
     async def sp_ilvl(self, instance, value):
         await self.rb_ilvl.write(value)
+        if self.sts_out_ena.value == 1:
+            await self.rb_meas_v.write(value)
+        else:
+            await self.rb_meas_v.write(0)
         return value
 
     @sp_meas_irang.putter
@@ -119,3 +131,19 @@ class K2600B(PVGroup):
     @sp_sour_irang.startup
     async def sp_sour_irang(self, instance, async_lib):
         await self.sts_sour_irang.write(instance.value)
+
+    @rb_meas_v.scan(period=0.1)
+    async def rb_meas_v(self, instance, async_lib):
+        if self.sts_out_ena.value == 1:
+            value = np.random.normal(self.rb_vlvl.value, 0.1)
+            await instance.write(value)
+        else:
+            await instance.write(0)
+
+    @rb_meas_i.scan(period=0.1)
+    async def rb_meas_i(self, instance, async_lib):
+        if self.sts_out_ena.value == 1:
+            value = np.random.normal(self.rb_ilvl.value, 0.1)
+            await instance.write(value * 1e-9)
+        else:
+            await instance.write(0)
